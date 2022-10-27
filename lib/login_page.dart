0c4df294467d8633466e9968941086998f2d4c22
Maylon <maylongatodemais25@gmail.com>
2022-10-27 10:50:51 -0300

    mgbngjnykny

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_biblioteca_planilha/app_config.dart';
 import 'package:flutter/material.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({ Key? key }) : super(key: key);
@@ -10,7 +11,21 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+   body: SafeArea(
+    child: SingleChildScrollView(
+      child: Container(
+       height:altura(context) - MediaQuery.of(context).padding.top,
+      child: Stack(
+        children: [
+ Padding(padding: EdgeInsets.symmetric(horizontal: 45),
+ child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.center,),),
+ SizedBox(height: 42,),
+ Image.asset('lib/images/granjas.png')
+      ],
+      ),
+      ),
+    ),
+   ),   
     );
   }
 }
