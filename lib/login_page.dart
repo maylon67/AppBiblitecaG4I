1c15a549ee1d40cc5ccdfa5ab03d33e57a484303
Maylon <maylongatodemais25@gmail.com>
2022-10-27 11:31:25 -0300

    nfjbngjb ngjng

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_biblioteca_planilha/app_config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({ Key? key }) : super(key: key);
 
@@ -15,12 +16,24 @@ class _LoginPageState extends State<LoginPage> {
     child: SingleChildScrollView(
       child: Container(
        height:altura(context) - MediaQuery.of(context).padding.top,
-      child: Stack(
+      child: Column(
         children: [
  Padding(padding: EdgeInsets.symmetric(horizontal: 45),
  child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.center,),),
- SizedBox(height: 42,),
- Image.asset('lib/images/granjas.png')
+ Padding(
+   padding: EdgeInsets.symmetric(horizontal: 145,vertical: 45),
+   child: SizedBox(height: 110,width:135,child: Image.asset('lib/images/granjasss.png')),
+ ),
+ Padding(padding: EdgeInsets.symmetric(vertical: 12)),
+ Padding(
+   padding: const EdgeInsets.symmetric(vertical: 25,horizontal: 8),
+   child: Text('Seja bem vindo ao App da Biblioteca das Granjas 4 Irmãos.',style: GoogleFonts.bebasNeue(fontSize: 39,color: Colors.green),textAlign: TextAlign.center,),
+ ),
+ Padding(padding: EdgeInsets.symmetric(vertical: 25)),
+ RaisedButton(
+  onPressed: (() {}),
+  
+ ),
       ],
       ),
       ),
