22909d2b50eb97f6343a6fe4c2f62447e5a83f68
Maylon <maylongatodemais25@gmail.com>
2022-10-27 10:03:44 -0300

    bglnmknm

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:app_biblioteca_planilha/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+home: LoginPage(),
+debugShowCheckedModeBanner: false,  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
