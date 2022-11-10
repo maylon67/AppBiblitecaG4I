74d1f24556dde98365b2d6f78d92ebcf6f9405a3
Maylon <maylongatodemais25@gmail.com>
2022-11-10 11:58:08 -0300

    gjhgjhbjgbgh

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:app_biblioteca_planilha/app_config.dart';
 import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttericon/elusive_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:fluttericon/linecons_icons.dart';
+import 'package:fluttericon/entypo_icons.dart';
+import 'package:fluttericon/modern_pictograms_icons.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -285,6 +288,406 @@ class _HomePageState extends State<HomePage> {
             Padding(padding: EdgeInsets.symmetric(vertical: 23)),
             Icon(Linecons.money,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
             Text('Economia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.pc_display,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Tecnologia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(Entypo.doc_text,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Política',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),),
+          ],
+         ),
+        ),
+          ),
+           GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(FontAwesome.book,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Poesia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+           GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.calculator,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Contabilidad',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+           GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(FontAwesome5.brain,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Pscícologia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(FontAwesome5.quote_right,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Filosofia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.shop,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Marketing',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(Elusive.lightbulb,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Startups',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(ModernPictograms.chart,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Gestão',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(FontAwesome.chart_bar,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Admin',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(FontAwesome5.question,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('AutoConhec',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(Entypo.language,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Estrangeiro',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.people,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Sociologia',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(Entypo.book,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Ficção',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.list_nested,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Diversos',style: GoogleFonts.chakraPetch(
+              color: Color.fromARGB(255, 7, 7, 7),
+              fontSize: 26
+            ),)
+          ],
+         ),
+        ),
+          ),
+          GestureDetector(
+           // onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AnimesPage(),)),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(23.0),
+          side: BorderSide(
+            color: Color.fromARGB(255, 10, 90, 22),
+            width: 4.5
+          )
+        ),
+        shadowColor: Color.fromARGB(255, 11, 74, 16),
+        elevation: 17.6,
+         child: Column(
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 23)),
+            Icon(BootstrapIcons.file_earmark_person,size: 90,color: Color.fromARGB(255, 11, 77, 21),),
+            Text('Auto-Bio',style: GoogleFonts.chakraPetch(
               color: Color.fromARGB(255, 7, 7, 7),
               fontSize: 26
             ),)
